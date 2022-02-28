a1c5b2ffd49123ad28e165327213a86e1c89199d
yukinobu fujii <y.f.mynovember0818@gmail.comgit config --global user.email y.f.mynovember0818@gmail.com>
2022-03-01 07:47:19 +0900

    課題完了

--- kadai1-2.js
+++ kadai1-2.js
@@ -1,1 +1,11 @@
-let 
+const quizCapitalJapan = "日本の首都は？";
+const correctAnswer = "正解";
+const incorrectAnswer = "不正解";
+let flg=true;
+while(flg){
+  if(flg=(window.prompt(quizCapitalJapan)!== "東京")){
+    window.alert(`${incorrectAnswer}です！`);
+    continue;
+  }
+  window.alert(`${correctAnswer}です！`);
+}
