ecd431c492f81f22b86ca9678e7581cb153c680d
yukinobu fujii <y.f.mynovember0818@gmail.comgit config --global user.email y.f.mynovember0818@gmail.com>
2022-03-09 20:18:34 +0900

    課題修正

--- kadai1-2.js
+++ kadai1-2.js
@@ -1,11 +1,13 @@
-const quizCapitalJapan = "日本の首都は？";
-const correctAnswer = "正解";
-const incorrectAnswer = "不正解";
-let flg=true;
-while(flg){
-  if(flg=(window.prompt(quizCapitalJapan)!== "東京")){
-    window.alert(`${incorrectAnswer}です！`);
-    continue;
+const quizCapitalJapan = '日本の首都は？';
+const correctAnswer = '正解';
+const incorrectAnswer = '不正解';
+//let flg=true;
+//while(flg){
+while(flg=true){
+  if(flg=(prompt(quizCapitalJapan)=== '東京')){
+    alert(`${correctAnswer}です！`);
+    break;
+  }else{
+  alert(`${incorrectAnswer}です！`);
   }
-  window.alert(`${correctAnswer}です！`);
 }
